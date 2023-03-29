b24353c7bdbe79722fcbecb8135c759119717690
elinarnten <elin.arnten@hotmail.com>
2023-03-29 14:13:40 +0200

    commented the broken iframe link

--- src/components/departures.jsx
+++ src/components/departures.jsx
@@ -5,12 +5,12 @@ function Departures() {
         style={{ width: "100%", height: "100vh" }}
         src="https://avgangstavla.vasttrafik.se/?source=vasttrafikse-depatureboardlinkgenerator&stopAreaGid=9021014007172000"
       ></iframe>
-      <iframe
+      {/* <iframe
         style={{ width: "100%", height: "100%" }}
         src="https://trafiken.nu/goteborg/#57.699784,11.980021,7?fullscreen"
-        frameborder="0"
+        frameBorder="0"
         allowFullScreen
-      ></iframe>
+      ></iframe> */}
     </div>
   );
 }
