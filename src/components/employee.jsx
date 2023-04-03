31bf07c6217fc96bcead369082bc7901e9fde3c0
elinarnten <elin.arnten@hotmail.com>
2023-04-03 11:07:57 +0200

    removed object and imported json data

--- src/components/employee.jsx
+++ src/components/employee.jsx
@@ -1,116 +1,5 @@
+import employeeData from "./employeeData.json";
 function Employee() {
-  const employee = [
-    {
-      id: 1,
-      name: "Pierre Alenbrink",
-      location: "Astra Zeneca",
-    },
-    {
-      id: 2,
-      name: "Veronica Jansson",
-      location: "Volvo",
-    },
-    {
-      id: 3,
-      name: "Tobias Andblad",
-      location: "Volvo",
-    },
-    {
-      id: 4,
-      name: "David Engvall",
-      location: "Volvo",
-    },
-    {
-      id: 5,
-      name: "Mark Tennyson",
-      location: "Husqvarna",
-    },
-    {
-      id: 6,
-      name: "Gisela Gutierrez Meza",
-      location: "Saab",
-    },
-    {
-      id: 7,
-      name: "Hans Börjesson",
-      location: "Saab",
-    },
-    {
-      id: 8,
-      name: "Daniel Nilsson",
-      location: "Volvo",
-    },
-    {
-      id: 9,
-      name: "Tobias Gilbertsson",
-      location: "Volvo",
-    },
-    {
-      id: 10,
-      name: "Jakob Nilsson",
-      location: "Volvo",
-    },
-    {
-      id: 11,
-      name: "Emilia Moberg",
-      location: "Salus Mea",
-    },
-    {
-      id: 12,
-      name: "Klaus Nohlås",
-      location: "Testscouts HQ",
-    },
-    {
-      id: 13,
-      name: "Jakob Pettersson",
-      location: "Volvo",
-    },
-    {
-      id: 14,
-      name: "Bengt Augustsson",
-      location: "Volvo",
-    },
-    {
-      id: 15,
-      name: "Maria Celovska",
-      location: "Volvo",
-    },
-    {
-      id: 16,
-      name: "Ruben Sandström",
-      location: "Volvo",
-    },
-    {
-      id: 17,
-      name: "Karin Dahlberg",
-      location: "Stamcellslabbet Sahlgrenska",
-    },
-    {
-      id: 18,
-      name: "Martin Drugge",
-      location: "Alstom",
-    },
-    {
-      id: 19,
-      name: "Mikael Engvall",
-      location: "CEVT",
-    },
-    {
-      id: 20,
-      name: "Henrik Simpanen",
-      location: "Husqvarna",
-    },
-    {
-      id: 21,
-      name: "Mattias Torstensson",
-      location: "Husqvarna",
-    },
-    {
-      id: 22,
-      name: "Robert Hennersten-Manley",
-      location: "Alstom",
-    },
-  ];
   return (
     <div
       style={{
@@ -129,7 +18,7 @@ function Employee() {
           justifyContent: "center",
         }}
       >
-        {employee.map((employee) => {
+        {employeeData.employeeData.map((employeeData) => {
           return (
             <div
               style={{
@@ -138,10 +27,10 @@ function Employee() {
                 padding: "1rem",
                 width: "auto",
               }}
-              key={employee.id}
+              key={employeeData.id}
             >
               <h2>
-                {employee.name}: {employee.location}
+                {employeeData.name}: {employeeData.location}
               </h2>
             </div>
           );
