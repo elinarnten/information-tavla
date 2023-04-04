1e79c27cbd32b28f5ff308fe92ce37e70b91b569
elinarnten <elin.arnten@hotmail.com>
2023-04-04 14:05:09 +0200

    congratulations message will be displayed matching someones bday

--- src/components/birthdays.jsx
+++ src/components/birthdays.jsx
@@ -1,11 +1,23 @@
+// import { useEffect, useState } from "react";
 import employeeData from "./employeeData.json";
 
-export const Birthdays = () => {
+function Birthdays() {
+  const d = new Date().toISOString();
+  console.log(d);
+
+  const today = d.slice(5, 10);
+  console.log(today);
+
   return (
     <div>
-      {employeeData.employeeData.map((employeeData) => {
-        return <div></div>;
-      })}
+      {employeeData.employeeData.map((employeeData) => (
+        <div>
+          {employeeData.bDay === today ? (
+            <p>Grattis! {employeeData.name} </p>
+          ) : null}
+        </div>
+      ))}
     </div>
   );
-};
+}
+export default Birthdays;
