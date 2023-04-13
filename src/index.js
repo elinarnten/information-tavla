6c831af55730ad2816a317b86634d4ff98b5edca
elinarnten <elin.arnten@hotmail.com>
2023-04-13 09:23:39 +0200

    removed commented lines

--- src/index.js
+++ src/index.js
@@ -2,28 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Welcome from "./components/welcome";
 // import Weather from "./components/weather";
-import Employee from "./components/employee";
 // import News from "./components/news";
 // import Departures from "./components/departures";
-import NewEmployee from "./components/newEmployee";
-import Birthdays from "./components/birthdays";
+import Carousel from "./components/carousel";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Welcome />
+    <Carousel />
     {/* <Weather /> */}
-    <Employee />
     {/* <News /> */}
     {/* <Departures /> */}
-    <NewEmployee />
-    <Birthdays />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
