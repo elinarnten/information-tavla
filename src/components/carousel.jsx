d8dd536750c9cab935bbc930ecd5194f56c54cd8
elinarnten <elin.arnten@hotmail.com>
2023-04-13 14:40:03 +0200

    New component. Gather all components in sthis component with carousel function

---
+++ src/components/carousel.jsx
@@ -0,0 +1,46 @@
+import Slider from "react-slick";
+import Birthdays from "./birthdays";
+import Employee from "./employee";
+import NewEmployee from "./newEmployee";
+import Welcome from "./welcome";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function Carousel() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <Slider {...settings}>
+      <div>
+        <Welcome />
+      </div>
+      <div>
+        <Employee />
+      </div>
+      <div>
+        <NewEmployee />
+      </div>
+      <div>
+        <Birthdays />
+      </div>
+    </Slider>
+  );
+}
+export default Carousel;
