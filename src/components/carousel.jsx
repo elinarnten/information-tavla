1ed5434d3d50e9d891fa01c48a600186869f5e15
elinarnten <elin.arnten@hotmail.com>
2023-04-24 08:46:50 +0200

    edit the carousel settings

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -8,14 +8,15 @@ import "slick-carousel/slick/slick-theme.css";
 
 function Carousel() {
   var settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     fade: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
+
     responsive: [
       {
         breakpoint: 768,
